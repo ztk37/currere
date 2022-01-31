615a0a74abd701e69303c7ba69f4742bdda92e63
ztk37 <ztk37.contact@gmail.com>
2022-01-31 21:09:39 +0100

    update

--- lib/Currere/Monad.hs
+++ lib/Currere/Monad.hs
@@ -2,25 +2,49 @@
 
 module Currere.Monad where
 
-import Control.Monad.Reader
+import           Data.Map (Map)
+import qualified Data.Map as Map
+
+import           Control.Monad.Reader
+
+data Connection = Connection
+  deriving (Eq, Show)
+
+type Config = Map String String
+type Env = [(String, String)]
 
 data Ctx = Ctx
-  deriving (Eq, Show)
+  { _conn :: !Connection
+  , _config :: !Config
+  , _env :: !Env
+  }
+
+initCtx :: IO Ctx
+initCtx = Ctx
+  <$> return Connection
+  <*> return Map.empty
+  <*> return []
 
 newtype CurrereM a = CurrereM
   { unCurrereM :: ReaderT Ctx IO a
   } deriving ( Functor
              , Applicative
              , Monad
-             , MonadReader Ctx
              , MonadIO
+             , MonadReader Ctx
              )
 
-runCurrereM :: CurrereM a -> Ctx -> IO a
-runCurrereM = runReaderT . unCurrereM
+runCurrereM :: Ctx -> CurrereM a -> IO a
+runCurrereM ctx = flip runReaderT ctx . unCurrereM
 
-askCtx :: CurrereM Ctx
-askCtx = ask
+useConnection :: CurrereM Connection
+useConnection = _conn <$> ask
+
+useConfig :: CurrereM Config
+useConfig = _config <$> ask
+
+useEnv :: CurrereM Env
+useEnv = _env <$> ask
 
 liftCurrereM :: IO a -> CurrereM a
 liftCurrereM = liftIO
--- lib/Currere/Run.hs
+++ lib/Currere/Run.hs
@@ -8,9 +8,15 @@ import System.Environment (getArgs)
 import Currere.Monad
 
 mainWithArgs :: [String] -> IO ()
-mainWithArgs args = flip runCurrereM Ctx $ do
-  env <- askCtx
-  liftCurrereM $ print env
+mainWithArgs _ = do
+  ctx <- initCtx
+  
+  runCurrereM ctx $ do
+    conn <- useConnection
+    config <- useConfig
+    env <- useEnv
+
+    liftCurrereM $ print (conn, config, env)
 
 defaultMain :: IO ()
 defaultMain = getArgs >>= mainWithArgs
